7ba5e5682ae6e822de1474021c190543bdae9061
Dorota Jarecka <djarecka@gmail.com>
2020-04-23 13:11:44 -0400

    adding aouth0

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,30 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-//import { login, isAuthenticated, getProfile } from "../utils/auth"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 import { Router } from "@reach/router"
 
-// nie wiem czy cos musi byc w tych <p>
-// const HomeAccount = ({ user }) => {
-//   return <p>Hi, {user.name ? user.name : "friend"}!</p>
-// }
-const HomeAccount = () => <p></p>//({user}) => <p>{user.name ? user.name : "unknown"}</p>
+const HomeAccount = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+//const HomeAccount = () => <p></p>//({user}) => <p>{user.name ? user.name : "unknown"}</p>
 //const Diary = () => <p></p>
 
 class Account extends React.Component {
   render() {
     const { data } = this.props
-  // if (!isAuthenticated()) {
-  //   login()
-  //   return <p>Redirecting to login...</p>
-  // }
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
 
-  // const user = getProfile()
+  const user = getProfile()
 
   return (
   <>
    <Router>
-    <HomeAccount path="/account"/>
+    <HomeAccount path="/account" user={user} />
   </Router>
 
   <Layout location={this.props.location}>
